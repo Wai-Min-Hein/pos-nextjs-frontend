6feb8ff886503b4bf80514d712ba7029be446907
Wai-Min-Hein <wai06276@gmail.com>
2025-01-04 23:54:56 +0630

    adjust next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*', // Match all routes starting with /api/
+          destination: 'https://expresspos.vercel.app/:path*', // Proxy to the backend
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
